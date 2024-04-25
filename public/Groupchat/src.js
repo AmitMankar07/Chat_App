2a35a31bba58db43a91ba2db46d3f983d8171864
AmitMankar07 <amitmankar071@gmail.com>
2024-04-25 18:25:15 +0530

    mking chat app real time

--- public/Groupchat/src.js
+++ public/Groupchat/src.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded',async () => {
+    
     let chatMessages = document.getElementById('chat-messages'); // Initialize chatMessages here
+    let latestMessageId = null;
 
+    async function fetchMessages() {
     try {
         const token = localStorage.getItem('token');
         if (!token) {
@@ -8,24 +11,32 @@ document.addEventListener('DOMContentLoaded',async () => {
             return;
         }
         
-        const response = await axios.get('/chats/getMessages', {
-            headers: {
-                'Authorization': `${token}`
+        let url = '/chats/getMessages';
+            if (latestMessageId) {
+                url += `?latestMessageId=${latestMessageId}`;
             }
-        });
+
+            const response = await axios.get(url, {
+                headers: {
+                    'Authorization': `${token}`
+                }
+            });
 
         if (response.status === 200) {
             const messages = response.data.messages;
             messages.forEach(message => {
                 addMessage(`${message.name}: ${message.message}`, message.isCurrentUser);
             });
+            if (messages.length > 0) {
+                latestMessageId = messages[messages.length - 1].id;
+            }
         } else {
             console.error('Failed to fetch messages');
         }
     } catch (error) {
         console.error('Error fetching messages:', error.response ? error.response.data : error.message);
     }
-
+    }
     const messageInput = document.getElementById('message-input');
     const sendButton = document.getElementById('send-button');
 
@@ -79,4 +90,20 @@ document.addEventListener('DOMContentLoaded',async () => {
 
     // Event listener for form submission
     sendButton.addEventListener('click', handleSubmit);
+    
+    await fetchMessages();
+    
+    setInterval(fetchMessages, 10000);
+
+    const signOutButton = document.getElementById('sign-out-button');
+
+    // Function to handle sign out
+    function handleSignOut() {
+        // Clear token from localStorage
+        localStorage.removeItem('token');
+        // Redirect to login page
+        window.location.href = '/Login/index.html'; // Adjust the path as per your file structure
+    }
+    signOutButton.addEventListener('click', handleSignOut);
+
 });
